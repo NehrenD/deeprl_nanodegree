cfc32ff83f6eb9b68cdbf770debc6c174088b5d5
NehrenD <thenehrens@yahoo.com>
2020-07-13 16:19:09 +0000

    Solved Notebook and evidence

--- tennis/maddpg_model.py
+++ tennis/maddpg_model.py
@@ -1,7 +1,11 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+import random
 import numpy as np
-import ptan
+from collections import deque,namedtuple
 
 #UTILS
 class Config():
@@ -25,3 +29,108 @@ class Config():
     GAMMA = 0.99
 
     device = 'cpu'
+
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
+
+
+class Actor(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, fc1_units=256, fc2_units=128):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(Actor, self).__init__()
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state):
+        """Build an actor (policy) network that maps states -> actions."""
+        x = F.elu(self.fc1(state))
+        x = F.elu(self.fc2(x))
+        return torch.tanh(self.fc3(x))
+
+
+class Critic(nn.Module):
+    """Critic (Value) Model."""
+
+    def __init__(self, full_state_size, full_action_size, fcs1_units=256, fc2_units=128):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of both agents states
+            action_size (int): Dimension of both agents actions
+            fcs1_units (int): Number of nodes in the first hidden layer
+            fc2_units (int): Number of nodes in the second hidden layer
+        """
+        super(Critic, self).__init__()
+        self.fcs1 = nn.Linear(full_state_size, fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units+full_action_size, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, full_state, full_action):
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        xs = F.elu(self.fcs1(full_state))
+        x = torch.cat((xs, full_action), dim=1)
+        x = F.elu(self.fc2(x))
+        return self.fc3(x)
+
+class ReplayBuffer(object):
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, buffer_size, batch_size,device):
+        """Initialize a ReplayBuffer object.
+        Params
+        ======
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+        """
+        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["full_state", "state", "action", "reward", \
+                                                                "full_next_state", "next_state", "done"])
+        self.device = device
+
+    def add(self, full_state, state, action, reward, full_next_state, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(full_state, state, action, reward, full_next_state, next_state, done)
+        self.memory.append(e)
+    
+    def sample(self):
+        """Randomly sample a batch of experiences from memory."""
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        full_states = torch.from_numpy(np.array([e.full_state for e in experiences if e is not None])).float().to(self.device)
+        states = torch.from_numpy(np.array([e.state for e in experiences if e is not None])).float().to(self.device)
+        actions = torch.from_numpy(np.array([e.action for e in experiences if e is not None])).float().to(self.device)
+        rewards = torch.from_numpy(np.array([e.reward for e in experiences if e is not None])).float().to(self.device)
+        full_next_states = torch.from_numpy(np.array([e.full_next_state for e in experiences if e is not None])).float().to(self.device)
+        next_states = torch.from_numpy(np.array([e.next_state for e in experiences if e is not None])).float().to(self.device)
+        dones = torch.from_numpy(np.array([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(self.device)
+
+        return (full_states, states, actions, rewards, full_next_states, next_states, dones)
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
